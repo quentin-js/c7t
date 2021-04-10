10e60d8f0a6790d3b3a24b5632c562daa52b06a0
KelgorEinhskor <quentin.sinig@strapi.io>
2021-04-10 12:11:05 +0200

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '38d017cea31ecb1e225172eb394ae2c4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e123ff33-35a9-40fb-8b10-210a1d05f90e'
+};
